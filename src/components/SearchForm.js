b2ac80e68e84a6cae6982ebfc703035cf1d435fe
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-13 12:19:11 -0500

    [+] SearchForm > setState: searchTerm & onChange/onSubmit

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchForm = () => {
+
+    /** Hook: Define State */
+    const [ searchTerm, setSearchTerm ] = useState( '' );
+
+    /** Methods */
+    const submitSearch = event => {
+        event .preventDefault();
+        console .log( 'Termino de busqueda', searchTerm );
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={ submitSearch }
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input 
@@ -10,11 +22,12 @@ const SearchForm = () => {
                         className="form-control form-control-lg" 
                         id="search" 
                         aria-describedby="searchHelp"
+                        onChange={ event => setSearchTerm( event .target .value ) }
                     />
                     <small id="searchHelp" className="form-text text-muted">Escribe una palabra que describa tu búsqueda.</small>
                 </div>
                 <div className="form-group col-md-4">
-                    <button type="button" className="btn btn-warning btn-lg btn-block">Buscar</button>
+                    <button type="submit" className="btn btn-warning btn-lg btn-block">Buscar</button>
                 </div>
             </div>
         </form>
