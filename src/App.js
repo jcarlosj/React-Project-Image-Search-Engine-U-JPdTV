11ed1bb149e307714e9d1c9ca99e886d35629b4b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-13 14:30:35 -0500

    [+] App > useState: Query API & Update the State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchForm from './components/SearchForm';
 
 function App() {
 
   /** Hook: Define State */
-  const [ term, setTerm ] = useState( '' );
+  const [ term, setTerm ] = useState( '' ),
+        [ apiData, setApiData ] = useState( {} );
+
+  /** Hook: Traking State */
+  useEffect( () => {
+
+    const getApiData = async () => {
+
+      /** Validate State */
+      if( term === '' ) return null;
+
+      /** Request API */
+      const 
+        resultsPerPage = 18,
+        apiKey = '9109568-ff204efa6ca38fb149b4fe7d6',
+        url = `https://pixabay.com/api/?key=${ apiKey }&q=${ term }&per_page=${ resultsPerPage }`,
+        response = await fetch( url ),
+        data = await response .json();
+
+        console .log( 'Data Pixabay', data );
+        setApiData( data );
+
+    }
+    getApiData();
+
+    
+
+
+
+  }, [ term ] );
 
   return (
     <div className="container mt-3">
